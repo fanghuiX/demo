4d2af38318ad0e8416f0246a3d2dd234f59988e7
fanghui <fanghuiplus@163.com>
2021-11-04 17:17:40 +0800

    kafka服务器连接监听

--- src/main/java/com/example/demo/kafka/KafkaConsumer.java
+++ src/main/java/com/example/demo/kafka/KafkaConsumer.java
@@ -2,6 +2,7 @@ package com.example.demo.kafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
 
-//    @KafkaListener(topics = {"test"})
-//    public void listen(ConsumerRecord<?, ?> record) {
-//        return;
-//    }
+    @KafkaListener(topics = {"test"})
+    public void listenTest(ConsumerRecord<String, String> record) {
+        System.out.println("topic: " + record.topic() + "; value: " + record.value() + "; offset: " + record.offset());
+        return;
+    }
+
+    @KafkaListener(topics = {"fanghui"})
+    public void listenFanghui(ConsumerRecord<String, String> record) {
+        System.out.println("topic: " + record.topic() + "; value: " + record.value() + "; offset: " + record.offset());
+        return;
+    }
 }
